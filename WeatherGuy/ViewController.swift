a2c2b454c0e68dc50c61b46e97870047832950a3
Sean <sean.mccalgan@gmail.com>
2018-06-13 14:35:33 +0200

    Initial Commit

---
+++ WeatherGuy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGuy
+//
+//  Created by Sean McCalgan on 2018/06/13.
+//  Copyright © 2018 Sean McCalgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
